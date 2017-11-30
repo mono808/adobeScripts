af0f4fc7f434d0c77cadd731e0a16581c90a8f7b
Jan Untiedt <mono808@gmail.com>
2017-11-30 22:37:35 +0100

    CleanUp, add simple illustrator scripts

---
+++ illustrator/Resize_Artboard_to_Selection.jsx
@@ -0,0 +1,13 @@
+﻿#target illustrator
+
+#includepath '../includes'
+#include 'illustrator_functions.jsx'
+
+if (app.documents.length > 0 && app.activeDocument.selection.length > 0) {
+    
+    var myDoc = app.activeDocument;
+    fit_artboard_to_selection(myDoc, myDoc.selection);
+
+} else {
+    alert('Bitte Pfade auswählen')
+};
