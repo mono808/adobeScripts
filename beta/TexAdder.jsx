b20e4ab560fbed4cacc0f55283d7292907bac5ee
Jan Untiedt <mono808@gmail.com>
2017-12-08 01:07:27 +0100

    texAdder chooses texLayers

--- beta/TexAdder.jsx
+++ beta/TexAdder.jsx
@@ -1,6 +1,8 @@
 ﻿function TexAdder () {
     var csroot = $.getenv("csroot");
     var texRoot = new Folder(csroot + "/Produktion/Druckvorstufe/textilien");
+    var doc = app.activeDocument;
+    var fixedLayers = /(Shirt|Front|Back|Naht|Tasche|Beutel)$/i;
         
     return {
         add_tex : function () {
@@ -11,13 +13,15 @@
                 var tempTex = this.get_tex_files(texFolders[i]);
                 allTexs = allTexs.concat(tempTex);
             }
-            var selectedTex = this.show_type_ahead(allTexs);
+            var selectedTex = this.show_type_ahead(allTexs, 'displayName');
+            var images = this.place_files_on_page(selectedTex);
+            this.choose_graphicLayers(images);
             
         },
     
         get_tex_folder : function () {
             var texFolders = texRoot.getFiles (function (a) {return a instanceof Folder});
-            var selectedFolders = this.show_type_ahead(texFolders);
+            var selectedFolders = this.show_type_ahead(texFolders, 'displayName');
             return selectedFolders;
         },
     
@@ -26,36 +30,100 @@
             return texFiles;
         },
 
-        create_names_array : function (array) {            
+        create_names_array : function (array, propertyToList) {            
             var names = [];
             for(var i = 0; i < array.length; i++) {
-                names.push(array[i].displayName);
+                names.push(array[i][propertyToList]);
             }
             return names;
         },
     
-        get_member : function (haystack, needle) {
+        get_member : function (haystack, needle, propertyToCheck) {
             for (var i = 0; i < haystack.length; i++) {
-                if(haystack[i].displayName == needle) {
+                var val = propertyToCheck ? haystack[i][propertyToCheck] : haystack[i];                   
+                if( val == needle) {
                     return haystack[i];
+                }
+            }
+            return null;
+        },
+
+        place_files_on_page : function (myFiles) {
+            var images = [];
+            for (var i = 0; i < myFiles.length; i++) {
+                var retval = doc.layoutWindows[0].activePage.place(myFiles[i], undefined, doc.activeLayer);
+                if(retval && retval.length > 0) images.push(retval[0]);
+            }
+            return images;
+        },
+
+        choose_graphicLayers : function (myImages)
+        {            
+            for (var i = 0; i < myImages.length; i++) {
+                var myImage = myImages[i];
+                
+                // filter out graphic files unsuitable for activating graphic layers
+                if (myImage.imageTypeName != "Photoshop" && myImage.imageTypeName != "Adobe PDF") continue;
+
+                // get ref to the parent object, otherwise the script will fuckup ...
+                var rect = myImage.parent;
+                var gLO = rect.graphics.item(0).graphicLayerOptions;
+
+                if (gLO.graphicLayers < 2) continue;
+
+                var layerNames = [];
+                for (var j = 0; j < gLO.graphicLayers.length; j++) {
+                    layerNames.push(gLO.graphicLayers.item(j).name);
+                    //$.writeln('j-gL ' + j + ': '  + gLO.graphicLayers.item(j).name + ' myImage ID: ' + myImage.id);
+                }
+
+                var selectedLayerNames = this.show_type_ahead(layerNames);
+                
+                for(var k = 0, maxK = gLO.graphicLayers.length; k < maxK; k += 1) {
+                    var gLO = rect.graphics.item(0).graphicLayerOptions;
+                    var gL = gLO.graphicLayers.item(k);
+                    
+                    if(!gL.isValid) continue;
+                    if(this.get_member(selectedLayerNames, gL.name)) {
+                        gL.currentVisibility = true;
+                    } else {
+                        gL.currentVisibility = false;                       
+                    }
                 }
             }
         },
 
-        show_type_ahead : function (array) {
-            var names = this.create_names_array(array);
+        filter_graphicLayers : function (gLayers) {
+            var filteredLayers = [];
+            for (var i = 0; i < gLayers.length; i++) {
+                if(!gLayers[i].name.match(fixedLayers)) {
+                    filteredLayers.push(gLayers[i]);
+                }
+            }
+            return filteredLayers.sort();
+        },
+    
+        show_type_ahead : function (array, propertyToList) {
+            if(propertyToList) {
+                var names = this.create_names_array(array, propertyToList);
+            } else {
+                var names = array;
+            }
             var selected = [];
             var temp;
+            var keyCount = 0;
             var w = new Window ('dialog {text: "Quick select", alignChildren: "fill"}');
             var entry = w.add ('edittext {active: true}');
             var dummy = w.add ('panel {alignChildren: "fill"}');
             var list = dummy.add ('listbox', [0,0,250,250], names, {multiselect: true});
             entry.onChanging = function ()
             {
+                keyCount++;
+                if(keyCount < 3) return;
                 var temp = entry.text;
                 var tempArray = [];
                 for (var i = 0; i < names.length; i++) {
-                    if (names[i].toLowerCase().indexOf (temp) == 0) {
+                    if (names[i].toLowerCase().indexOf(temp) == 0) {
                         tempArray.push (names[i]);
                      }
                     if (tempArray.length > 0) {
@@ -79,18 +147,21 @@
                         selected.push(list.selection.text);
                     }
                 }
-                var returnArray = [];
+                if(!propertyToList) return selected;
+
+                // if not simply returning the selected text, get the corresponding object
+                var returnArray = [];               
                 for (var i = 0; i < selected.length; i++) {
-                    returnArray.push(this.get_member(array, selected[i]));
+                    returnArray.push(this.get_member(array, selected[i], propertyToList));
                 }
                 return returnArray;
             }
-
             w.close();
         }
     };
 }
 
+#target indesign
 function main () {
     var texAdder = new TexAdder();
     texAdder.add_tex();
