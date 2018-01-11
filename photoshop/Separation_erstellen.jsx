19e5f0e7af96646cf0d20740e65f3d7f275d0200
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2018-01-11 17:57:46 +0100

    moved ps scripts to working folder

--- photoshop/Separation_erstellen.jsx
+++ photoshop/Separation_erstellen.jsx
@@ -18,22 +18,22 @@ function main () {
     var iaSwitch = new InteractSwitch();
     iaSwitch.set('none');
 
-    // var saveFile = pm.file('sepPs');
-    // var sep = new SepDocPS(app.activeDocument, saveFile);
-    // app.refresh();
-    // if(Window.confirm ('Doc ok?', false, 'Check Document')) sep.doc.close();
-    // app.activeDocument = baseDoc.doc;
-    
-    var styles = ['merged', 'layered'];
-
-    var infoText = 'Please choose the style of the PreviewFile:\r\r';
-    infoText += 'Layered -> SpotChannels are displayed as Layers, all colors are 100% opaque\r\r';
-    infoText += 'Merged  -> SpotChannels are merged to flat RGB File, better opacity simulation';
-    var style = new ButtonList('Choose Preview Style', infoText).show_dialog(styles);
-
-    var saveFile = pm.file('previewPs');
-    var preview = new PreviewDocPS(app.activeDocument, style, saveFile);
+    var saveFile = pm.file('sepPs');
+    var sep = new SepDocPS(app.activeDocument, saveFile);
     app.refresh();
+    //if(Window.confirm ('Doc ok?', false, 'Check Document')) sep.doc.close();
+    app.activeDocument = baseDoc.doc;
+    
+    // var styles = ['merged', 'layered'];
+
+    // var infoText = 'Please choose the style of the PreviewFile:\r\r';
+    // infoText += 'Layered -> SpotChannels are displayed as Layers, all colors are 100% opaque\r\r';
+    // infoText += 'Merged  -> SpotChannels are merged to flat RGB File, better opacity simulation';
+    // var style = new ButtonList('Choose Preview Style', infoText).show_dialog(styles);
+
+    // var saveFile = pm.file('previewPs');
+    // var preview = new PreviewDocPS(app.activeDocument, style, saveFile);
+    // app.refresh();
     //if(Window.confirm ('Doc ok?', false, 'Check Document')) preview.doc.close();
     //app.activeDocument = baseDoc.doc;
 
