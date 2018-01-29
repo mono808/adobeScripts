960f0f0906114bb77d1a04a8ebf23cb98b4b3373
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2018-01-29 17:56:56 +0100

    ub tester dev added

---
+++ beta/UBTester-Dev.jsx
@@ -0,0 +1,13 @@
+﻿#target indesign
+
+function main () {    
+    
+    #include '/c/repos/adobeScripts1/includes/UBTester.jsxinc'
+    
+    var doc = app.activeDocument;
+    var ubTester = new UBTester(doc);
+    ubTester.add_tester();
+    
+}
+
+main();
