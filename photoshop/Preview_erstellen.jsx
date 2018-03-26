276f878bebc36439836ad529112dd4ae1cad9634
Jan Untiedt <mono808@gmail.com>
2018-03-26 22:00:25 +0200

    updated to prototypal inheritance

--- photoshop/Preview_erstellen.jsx
+++ photoshop/Preview_erstellen.jsx
@@ -14,36 +14,18 @@ function main () {
     var job = new Job(app.activeDocument, true);
     var pm = new Pathmaker(job.nfo);
 
-    var baseDoc = new BaseDocPS(app.activeDocument);
-
     var iaSwitch = new InteractSwitch();
     iaSwitch.set('none');
 
-    // var saveFile = pm.file('sepPs');
-    // var sep = new SepDocPS(app.activeDocument, saveFile);
-    // app.refresh();
-    // if(Window.confirm ('Doc ok?', false, 'Check Document')) sep.doc.close();
-    // app.activeDocument = baseDoc.doc;
-    
-    var styles = ['merged', 'layered'];
-
-    var infoText = 'Please choose the style of the PreviewFile:\r\r';
-    infoText += 'Layered -> SpotChannels are displayed as Layers, all colors are 100% opaque\r\r';
-    infoText += 'Merged  -> SpotChannels are merged to flat RGB File, better opacity simulation';
-    var style = new ButtonList('Choose Preview Style', infoText).show_dialog(styles);
-
     var saveFile = pm.file('previewPs');
-    var preview = new PreviewDocPS(app.activeDocument, style, saveFile);
-    app.refresh();
-    //if(Window.confirm ('Doc ok?', false, 'Check Document')) preview.doc.close();
-    //app.activeDocument = baseDoc.doc;
 
-    //var saveFile = pm.file('previewPs');
-    //var preview = new PreviewDocPS(app.activeDocument, 'layered');
-    //app.refresh();
-    //if(Window.confirm ('Doc ok?', false, 'Check Document')) preview.doc.close();
-    //app.activeDocument = baseDoc.doc;
+    var previewObj = Object.create(previewDocPS);
+    previewObj.startDoc = app.activeDocument;
+    previewObj.make(saveFile);
+
+    app.refresh();
 
     iaSwitch.reset();
 }
+
 main();
