016cadfcb1fc0a0fd91d9f2957493b11a4c47c77
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2018-03-28 16:24:54 +0200

    sort base file according to parent folder as print / preview/ working

--- includes/MonoPrint.jsx
+++ includes/MonoPrint.jsx
@@ -34,25 +34,35 @@
 		}
 	};
 
-	
+	var parentFolder = aFile.parent;
+	if(parentFolder.name.indexOf('Druckdaten') != -1) {
+		var printsFolder = parentFolder.name;
+		if(aFile.name.indexOf('Film.indd') == -1)
+			this.print = aFile;
+		
+	} else if (parentFolder.name.indexOf('Previews') != -1) {
+		this.preview = aFile;
+	} else if (parentFolder.name.indexOf('Working') != -1) {
+		this.working = aFile;
+	}
 
-	var baseFolder = baseFolder;
+
+
 	var tagRegEx = /^([a-z0-9äüöß-]+)_(\d{1,3}x\d{1,3})?_?(SD|DTA|DTG|SUB|FLX|FLK|STK)_?(print|druck|sep|working|preview|film)?\.[a-z]{2,3}$/i;
-    // 817A15-012_[Logo2Z-14cm_Front]_Sep
     var tag15 = /(\d{1,5}(wme|ang|cs|a)\d\d-0\d\d)_\[([a-z0-9äüöß-]+)_([a-z0-9äüöß-]+)\]_(sep|film|entwurf)/i;
     var tag16 = /([a-z0-9äüöß-]+)_([0-9]{1,3}x[0-9]{1,3})_(sd|flx|flk|dta|dtg|stk|sub)/i;
     var tag17 = /([a-z0-9äüöß-]+)_(sd|flx|flk|dta|dtg|stk|sub)_(working|print|preview|druck|entwurf)/i;
 	
     this.tag = this.get_tag(aFile);
-    var printsFolder = this.tech ? 'druckdaten-' + this.tech : 'druckdaten';
+    if(printsFolder == undefined) var printsFolder = this.tech ? 'druckdaten-' + this.tech : 'druckdaten';
 
-	this.print   = this.get_tagged_files(printsFolder, this.tag + '_*');
+	if(!this.print) this.print = this.get_tagged_files(printsFolder, this.tag + '*');
     if(!this.print) this.print = this.choose_file_manually('Choose PrintFile to ' + aFile.name);
     
-	this.preview = this.get_tagged_files('previews', this.tag+'_Preview.*');
+	if(!this.preview) this.preview = this.get_tagged_files('previews', this.tag+'_Preview.*');
 	if(!this.preview) this.preview = this.choose_file_manually('Choose PreviewFile to ' + aFile.name);
 
-	this.film    = this.get_tagged_files('druckdaten-sd', '*'+this.tag+'_Film.indd');
+	if(!this.film) this.film    = this.get_tagged_files('druckdaten-sd', '*'+this.tag+'_Film.indd');
 	if(!this.film) this.film = this.choose_file_manually('Choose Film.indd to ' + aFile.name);
     
 	this.working = this.get_tagged_files('working', this.tag+'_Working.*');
