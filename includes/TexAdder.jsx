611e627b9da8a8ab28bdd125b7d0120ea9fe668a
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2018-12-06 14:52:06 +0100

    texadder gets files from all subfolder and matches the whole name, not just the beginning

--- includes/TexAdder.jsx
+++ includes/TexAdder.jsx
@@ -1,20 +1,42 @@
-﻿function TexAdder (doc) {
+﻿function find_files (dir)
+    {
+    return find_files_sub (dir, []);
+    }
+
+function find_files_sub (dir, array)
+    {
+    var f = Folder (dir).getFiles ("*.*");
+    for (var i = 0; i < f.length; i++)
+        {
+        if (f[i] instanceof Folder)
+            find_files_sub (f[i], array);
+        else
+            if (f[i] instanceof File)
+                array.push (f[i]);
+        }
+    return array;
+    }
+
+function TexAdder (doc) {
     var csroot = $.getenv("csroot");
     var texRoot = new Folder(csroot + "/Produktion/Druckvorstufe/textilien");
     var doc;
     //var fixedLayers = /(Shirt|Front|Back|Naht|Tasche|Beutel)$/i;
-    var fixedLayers = /(Shirt|Naht|Tasche|Beutel)$/i;
+    var fixedLayers = /(Shirt|Naht|Tasche|Beutel|Hintergrund)$/i;
         
     return {
         add_tex : function (targetDoc) {
             doc = targetDoc ? targetDoc : app.activeDocument;
             var texFolders = [];
+            /*
             var allTexs = [];
             texFolders = this.get_tex_folder();
             for (var i = 0; i < texFolders.length; i++) {
                 var tempTex = this.get_tex_files(texFolders[i]);
                 allTexs = allTexs.concat(tempTex);
-            }
+            } 
+            */
+            var allTexs = this.get_tex_files(texRoot);
             var selectedTex = this.show_type_ahead(allTexs, 'displayName');
             var images = this.place_files_on_page(selectedTex);
             this.choose_graphicLayers(images);            
@@ -37,10 +59,15 @@
             return selectedFolders;
         },
     
-        get_tex_files : function (fld) {
+        get_tex_files_bkp : function (fld) {
             var texFiles = fld.getFiles (function (a) {return a instanceof File});
             return texFiles;
         },
+
+        get_tex_files : function (dir) {
+            var texFiles = find_files (dir);
+            return texFiles;
+        },        
 
         create_names_array : function (array, propertyToList) {            
             var names = [];
@@ -147,11 +174,11 @@
             entry.onChanging = function ()
             {
                 keyCount++;
-                if(keyCount < 3) return;
+                if(keyCount < 4) return;
                 var temp = entry.text;
                 var tempArray = [];
                 for (var i = 0; i < names.length; i++) {
-                    if (names[i].toLowerCase().indexOf(temp) == 0) {
+                    if (names[i].toLowerCase().indexOf(temp) > -1) {
                         tempArray.push (names[i]);
                      }
                     if (tempArray.length > 0) {
@@ -163,6 +190,25 @@
                     }
                 }
             } // entry.onChanging
+
+/*             entry.onChange = function ()
+            {
+                var temp = entry.text;
+                var tempArray = [];
+                for (var i = 0; i < names.length; i++) {
+                    if (names[i].toLowerCase().indexOf(temp) >= 0) {
+                        tempArray.push (names[i]);
+                    }
+                    if (tempArray.length > 0) {
+                        // Create the new list with the same bounds as the one it will replace
+                        tempList = dummy.add ("listbox", list.bounds, tempArray, {scrolling: true});
+                        dummy.remove(list);
+                        list = tempList;
+                        list.selection = 0;
+                    }
+                }
+            } // entry.onChanging    */   
+
             w.add ('button {text: "Ok"}');
 
             if (w.show () != 2){
