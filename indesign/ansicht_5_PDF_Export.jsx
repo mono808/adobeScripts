d7090f85113c07b0879d384144ad207653dcc093
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2019-07-24 12:37:56 +0200

    removed unused stuff

--- indesign/ansicht_5_PDF_Export.jsx
+++ indesign/ansicht_5_PDF_Export.jsx
@@ -21,34 +21,35 @@ function main () {
 
     var myFlattenerPresetName = "mockupFlattener";
 
-    var tempFlattenerPreset = app.flattenerPresets.itemByName(myFlattenerPresetName);
-    if(tempFlattenerPreset != null){
-        tempFlattenerPreset.remove();
+    var myFlattenerPreset;
+    try{
+        myFlattenerPreset = app.flattenerPresets.itemByName(myFlattenerPresetName);
+        var check = myFlattenerPreset.name;    
+    } catch (e) {
+        myFlattenerPreset = app.flattenerPresets.add({name:myFlattenerPresetName});
     }
 
-    var myFlattenerPreset = app.flattenerPresets.add(
-    {
-      name:myFlattenerPresetName,
-      rasterVectorBalance: 15,
-      lineArtAndTextResolution: resolution*2.5,
-      gradientAndMeshResolution: resolution*2.5,
-      convertAllTextToOutlines: true,
-      convertAllStrokesToOutlines: true,
-      clipComplexRegions: true
-    });
+    with(myFlattenerPreset) {
+        rasterVectorBalance= FlattenerLevel.MEDIUM_LOW;
+        lineArtAndTextResolution= resolution*2.5;
+        gradientAndMeshResolution= resolution*2.5;
+        convertAllTextToOutlines= false;
+        convertAllStrokesToOutlines= false;
+        clipComplexRegions= true;
+    }
 
-    var myExportPreset;
     try{
-        myExportPreset = app.pdfExportPresets.itemByName('monosPDFExportPreset');
-        var check = myExportPreset.name;    
-    } catch (e) {
-        myExportPreset = app.pdfExportPresets.add();
-    }  
+        var check = app.pdfExportPresets.itemByName('monosPDFExportPreset').name;
+        app.pdfExportPresets.itemByName('monosPDFExportPreset').remove();
+    } catch (e) {}
+    finally {
+        var myExportPreset = app.pdfExportPresets.add();
+    } 
 
     with(myExportPreset) {
         name = 'monosPDFExportPreset'
-        acrobatCompatibility = AcrobatCompatibility.ACROBAT_7;
-        //appliedFlattenerPreset = myFlattenerPreset;
+        acrobatCompatibility = AcrobatCompatibility.ACROBAT_4;
+        appliedFlattenerPreset = myFlattenerPreset;
         cropImagesToFrames = true;
         exportGuidesAndGrids = false;
         exportLayers = false;
@@ -58,7 +59,7 @@ function main () {
         optimizePDF = false;
         pdfColorSpace = PDFColorSpace.RGB;
         viewPDF = true;
-        compressTextAndLineArt = true;  
+        compressTextAndLineArt = false;
 
         colorBitmapCompression = BitmapCompression.JPEG;
         colorBitmapQuality = CompressionQuality.HIGH;
@@ -77,39 +78,6 @@ function main () {
         monochromeBitmapSamplingDPI = resolution*2;
         thresholdToCompressMonochrome = resolution*2;    
     };
-
-
-//~     with(app.pdfExportPreferences) {
-//~         acrobatCompatibility = AcrobatCompatibility.ACROBAT_4;
-//~         appliedFlattenerPreset = myFlattenerPreset;
-//~         cropImagesToFrames = true;
-//~         exportGuidesAndGrids = false;
-//~         exportLayers = false;
-//~         exportNonprintingObjects = false;
-//~         generateThumbnails = true;
-//~         includeICCProfiles = true;
-//~         optimizePDF = true;
-//~         pdfColorSpace = PDFColorSpace.RGB;
-//~         viewPDF = true;
-
-//~         colorBitmapCompression = BitmapCompression.JPEG;
-//~         colorBitmapQuality = CompressionQuality.HIGH;
-//~         colorBitmapSampling = Sampling.BICUBIC_DOWNSAMPLE;
-//~         colorBitmapSamplingDPI = resolution;
-//~         thresholdToCompressColor = resolution;
-
-//~         grayscaleBitmapCompression = BitmapCompression.JPEG;
-//~         grayscaleBitmapQuality = CompressionQuality.HIGH;
-//~         grayscaleBitmapSampling = Sampling.BICUBIC_DOWNSAMPLE;
-//~         grayscaleBitmapSamplingDPI = resolution;
-//~         thresholdToCompressGray = resolution;
-
-//~         monochromeBitmapCompression = MonoBitmapCompression.CCIT4;
-//~         monochromeBitmapSampling = Sampling.BICUBIC_DOWNSAMPLE;
-//~         monochromeBitmapSamplingDPI = resolution*2;
-//~         thresholdToCompressMonochrome = resolution*2;
-//~     }
-
     
     var layerToggle = f_id.layerToggle(['Intern', 'HL'])
     layerToggle.hide();
