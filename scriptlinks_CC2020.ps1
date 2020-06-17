fed2a9dbf6d3f5cba478be70007c9abcfe3094db
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2020-06-17 10:06:11 +0200

    add cc2019 folder for scriptlinks

--- scriptlinks_CC2020.ps1
+++ scriptlinks_CC2020.ps1
@@ -5,3 +5,5 @@ New-Item -Path "C:\Program Files\Adobe\Adobe Photoshop CS6 (64 Bit)\Presets\Scri
 New-Item -Path "$env:USERPROFILE\AppData\Roaming\Adobe\InDesign\Version 15.0\de_DE\Scripts\Scripts Panel\scripts" -ItemType SymbolicLink -Value "$env:pcroot/adobeScripts/indesign" -Force
 New-Item -Path "C:\Program Files\Adobe\Adobe Illustrator 2020\Presets\de_DE\Skripten\scripts" -ItemType SymbolicLink -Value  "$env:pcroot/adobeScripts/illustrator" -Force
 New-Item -Path "C:\Program Files\Adobe\Adobe Photoshop 2020\Presets\Scripts\scripts" -ItemType SymbolicLink -Value  "$env:pcroot/adobeScripts/Photoshop" -Force
+
+New-Item -Path "C:\Program Files\Adobe\Adobe Illustrator CC 2019\Presets\de_DE\Skripten\scripts" -ItemType SymbolicLink -Value  "$env:pcroot/adobeScripts/illustrator" -Force
