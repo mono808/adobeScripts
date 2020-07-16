8e02e1a5090a127cb7a442596c66ce363ce34d7f
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2020-07-16 17:43:54 +0200

    add script for replace multilayer psd files with flattened & embeded jpgs

---
+++ indesign/flatten_textil_graphics.jsx
@@ -0,0 +1,177 @@
+﻿
+function sendScriptToPhotoshop (PSfunction, scriptArgs) {
+    var retval = true;
+
+    // Create the message object
+    var bt = new BridgeTalk();
+    var serializedArgs = scriptArgs.toSource();
+    $.writeln(serializedArgs);
+
+    bt.target = 'photoshop';
+    bt.body = "var SnpSentMessage = {}; SnpSentMessage.main = " + PSfunction.toString() + ";";
+    bt.body += "SnpSentMessage.main(";
+    bt.body += serializedArgs;
+    bt.body += ")";
+    
+    bt.onError = function(errObj)
+    {
+        $.writeln('BT returned an error:\n');
+        $.writeln(errObj.body);
+    }
+    
+    bt.onResult = function(resObj)
+    {
+        // The result of executing the code is the last line of the script that was executed in the target (Photoshop)
+        $.writeln('BridgeTalk result:');
+        $.writeln(resObj.body);
+        retval = eval(resObj.body);
+        $.writeln("result evaled to:");
+        $.writeln(retval);
+    }
+
+    // Send the message, wait max 30 sec for response
+    bt.send(30);
+    
+    return retval;
+}
+
+
+
+function read_file (aFile) {     
+  if(aFile && aFile instanceof File) {
+        aFile.open('r', undefined, undefined);
+        aFile.encoding = "UTF-8";      
+        aFile.lineFeed = "Windows";
+        var success = aFile.read();
+        aFile.close();
+        return success;
+  }
+}
+
+function get_graphicLayerNames (myImage) {
+    // get ref to the parent object, otherwise the script will fuckup ...
+    var graphicLayers = myImage.graphicLayerOptions.graphicLayers;
+    var activeLayerNames = [];
+    var gL;
+
+    for (var j=0, lenJ=graphicLayers.length; j < lenJ ; j++) {
+        gL = graphicLayers[j];
+        if(gL.currentVisibility === true) {
+            activeLayerNames.push(gL.name);
+        }
+    };
+
+    return activeLayerNames;
+}
+
+function changeImage(image, aFile) {
+    var rec = image.parent;
+    //image.remove();
+
+    if(aFile.constructor.name !== 'File') {
+        aFile = new File(decodeURI(aFile));
+    }
+    rec.place(aFile);
+    //rec.fit(FitOptions.CONTENT_TO_FRAME);
+    rec.allGraphics[0].itemLink.unlink();
+}
+
+function ps_run (myArgs) {
+    $.writeln(myArgs.toSource());
+
+    var sourceDoc = app.open(new File(decodeURI(myArgs.sourcePath)));
+    var path = sourceDoc.path;
+    var visibleOjectLayers = myArgs.visibleOjectLayers;
+    
+    var doc = sourceDoc.duplicate();
+
+    /* activate the correct layerSets */
+    var layer, name;
+    for (var i=0, len=doc.layers.length; i < len ; i++) {
+        layer = doc.layers[i];
+        for (var j=0, lenJ=visibleOjectLayers.length; j < lenJ ; j++) {
+            // only toggle layers which are at top level, since those are available from indesign graphicLayerObject
+            if(layer.parent.constructor.name === 'Document') {
+                if(layer.name === visibleOjectLayers[j]) {
+                    layer.visible = true;
+                    break;
+                } else {
+                    layer.visible = false;
+                }
+            }
+        }
+    }
+
+    doc.flatten();
+
+    var jpgOptions = new JPEGSaveOptions();
+    jpgOptions.embedColorProfile = true;
+    
+    var jpgFile = new File(decodeURI(myArgs.destPath));
+
+    doc.saveAs(jpgFile, jpgOptions);
+    doc.close(SaveOptions.DONOTSAVECHANGES);
+    sourceDoc.close(SaveOptions.DONOTSAVECHANGES);
+
+    var retval = encodeURI(jpgFile);
+    return retval.toSource();
+}
+
+function indd_run () {
+    if(app.selection.length > 0) {
+        for (var i=0, len=app.selection.length; i < len ; i++) {
+            var selItem = app.selection[i];
+            if(selItem.constructor.name === 'Rectangle') {
+                var graphic = selItem.graphics[0];
+            } else {
+                var graphic = selItem;
+            }
+
+            if(graphic.imageTypeName === 'Photoshop' || graphic.imageTypeName === 'PDF') {
+                var visibleOjectLayers = get_graphicLayerNames(graphic);
+                var sourcePath = graphic.itemLink.filePath;
+
+                /* create filename containing the visible object layers */
+                
+                var filename = sourcePath.substring(0, sourcePath.lastIndexOf('.'));
+                var sourceExtension = sourcePath.substring(sourcePath.lastIndexOf('.'), sourcePath.length-1);
+                var destExtension = '.jpg';
+                for (var i=0, len=visibleOjectLayers.length; i < len ; i++) {
+                filename += '_-_';
+                filename += visibleOjectLayers[i];
+                };
+
+                var destPath = filename + destExtension;
+
+                var btArgs = {};
+                btArgs.sourcePath = encodeURI(sourcePath);
+                btArgs.destPath = encodeURI(destPath);
+                btArgs.visibleOjectLayers = visibleOjectLayers;
+
+                var jpgFilePath = sendScriptToPhotoshop(ps_run, btArgs);
+                if(jpgFilePath) {
+                    var jpgFile = new File(decodeURI(jpgFilePath));
+                }
+                
+                if(jpgFile.exists) {
+                changeImage(graphic, jpgFile);
+                jpgFile.remove();
+                }
+            }
+        }
+    }
+}
+
+$.writeln('script started');
+if(BridgeTalk.appName == 'indesign') {
+    $.writeln('running in indesign');
+    indd_run();
+}
+
+// just for testing the photoshop script without sending it through bridgeTalk
+if(BridgeTalk.appName == 'photoshop') {
+    $.writeln('running in photoshop');
+    var args = eval({sourcePath:"%5C%5Ccs-server16.dc-krueger.local%5CCapriShare%5CProduktion%5CDruckvorstufe%5Ctextilien%5Csweats%5CStanley%20Cultivator.psd", destPath:"%5C%5Ccs-server16.dc-krueger.local%5CCapriShare%5CProduktion%5CDruckvorstufe%5Ctextilien%5Csweats%5CStanley%20Cultivator_-_French_Navy_Front.jpg", visibleOjectLayers:["French_Navy_Front"]});
+    // var args = eval({sourcePath:"%5C%5Ccs-server16%5Ccaprishare%5CProduktion%5CDruckvorstufe%5Ctextilien%5C!Dummies%5CT-Shirt%20M%C3%A4nner-Test.psd", destPath:"%5C%5Ccs-server16%5Ccaprishare%5CProduktion%5CDruckvorstufe%5Ctextilien%5C!Dummies%5CT-Shirt%20M%C3%A4nner-Test_-_ForestGreen_-_Shirt.jpg", visibleOjectLayers:["ForestGreen", "Shirt"]});
+    ps_run(args);
+}
