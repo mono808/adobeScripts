92e4dc4c843a1864ef3742244960c3ff7d5c230c
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2020-09-03 17:42:35 +0200

    add dev stuff

---
+++ dev/vsdebugger.js
@@ -0,0 +1,5 @@
+function main () {
+  console.log('1 2 3 testing');
+}
+
+main();
---
+++ panellinks.ps1
@@ -0,0 +1,1 @@
+New-Item -Path "$env:USERPROFILE\AppData\Roaming\Adobe\CEP\extensions\" -ItemType SymbolicLink -Value "$env:pcroot/adobeScripts/panels" -Force
