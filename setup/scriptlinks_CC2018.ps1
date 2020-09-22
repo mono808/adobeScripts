0c8ebc8a2ab7ea86f96fd2b483fd51535ca554b9
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2020-09-22 12:15:21 +0200

    set systemvars and scriptlinks

---
+++ setup/scriptlinks_CC2018.ps1
@@ -0,0 +1,3 @@
+New-Item -Path "$env:USERPROFILE\AppData\Roaming\Adobe\InDesign\Version 13.0\de_DE\Scripts\Scripts Panel\scripts" -ItemType SymbolicLink -Value "$env:pcroot/adobeScripts/indesign" -Force
+New-Item -Path "$Env:Programfiles\Adobe\Adobe Photoshop CC 2018\Presets\Scripts\scripts" -ItemType SymbolicLink -Value  "$env:pcroot/adobeScripts/Photoshop" -Force
+New-Item -Path "$Env:Programfiles\Adobe\Adobe Illustrator CC 2018\Presets\de_DE\Skripten\scripts" -ItemType SymbolicLink -Value  "$env:pcroot/adobeScripts/illustrator" -Force
