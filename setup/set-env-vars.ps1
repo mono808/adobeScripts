6cf3997946367a0d2f6c3af34cc5a5cd888cea90
Jan <mono808@gmail.com>
2020-10-06 12:41:14 +0200

    unify ps1 setup file, use wawiscript folder for the monkeys

--- setup/set-env-vars.ps1
+++
@@ -1,3 +0,0 @@
-[System.Environment]::SetEnvironmentVariable('JSINCLUDE', 'C:\capri-stuff\adobeScripts\includes')
-[System.Environment]::SetEnvironmentVariable('pcroot', 'C:\capri-stuff')
-[System.Environment]::SetEnvironmentVariable('csroot', '\\cs-server16\CapriShare')
