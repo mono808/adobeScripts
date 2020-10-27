c85c04880b61d5fefe2f31f30bae870f2e26fa18
mono808 <35834167+mono808@users.noreply.github.com>
2020-10-27 10:03:14 +0100

    Revert "remove cs6 stuff"

---
+++ dev/MonoGraphic_Dev.jsx
@@ -0,0 +1,39 @@
+﻿#target indesign
+function main () {
+
+     
+    #include 'augment_objects.jsx'
+    #include 'Job.jsx'
+    #include 'JobFolder.jsx'
+    #include 'MonoNamer.jsx'
+    #include 'MonoFilm.jsx'
+    #include 'MonoMockup.jsx'
+    #include 'MonoGraphic.jsx'
+    #include 'MonoTextil.jsx'
+    #include 'MonoPrint.jsx'
+    #include 'Pathmaker.jsx'
+    #include 'MonoSep.jsx'
+    #include 'Typeahead.jsx'
+    #include 'TexAdder.jsx'
+    
+    var monoMockup = new MonoMockup();
+    monoMockup.init(app.activeDocument);
+    
+    var myPage = app.activeWindow.activePage;
+    var monoGraphics = monoMockup.get_monoGraphics(myPage, monoMockup.layers.prints);
+    
+    var mG = monoGraphics[0];
+    
+    var textil = mG.get_textil();
+    var textilColor = mG.get_textil_color();
+    var textilName = mG.get_textil_name();
+    var width = mG.get_width();
+    var filmFile = mG.get_file('film');
+    var stand = mG.get_stand();
+    var colors = mG.get_colors();
+    var sizesMatch = mG.check_size();
+    var order = mG.get_order();
+    var id = mG.get_id();
+
+}
+main();
---
+++ dev/dialogTester.jsx
@@ -0,0 +1,65 @@
+﻿    var exportStyleDialog = function () {
+        var returnValue = {
+            preset: null,
+            neutral: null
+        };
+
+        var activeWindow = app.activeWindow;
+        var width = activeWindow.bounds[3]-activeWindow.bounds[1];
+        centerWidth = activeWindow.bounds[1] + width/2;
+        var height = activeWindow.bounds[2]-activeWindow.bounds[0];
+        centerHeight = activeWindow.bounds[0] + height/2;
+        
+        var dialogWidth = 425;
+        var dialogHeight = 400;
+        var dLeft = centerWidth-dialogWidth/2;
+        var dRight = centerWidth+dialogWidth/2;
+        var dTop = centerHeight-dialogHeight/2;
+        var dBottom = centerHeight+dialogHeight/2;
+
+
+        //var win = new Window("dialog", "Neutral Ansicht erstellen?",[dLeft,dTop,dRight,dBottom]);
+        var win = new Window("dialog", "Neutral Ansicht erstellen?",undefined);
+        this.windowRef = win;
+        win.presetGroup = win.add("Group");
+        win.styleGroup = win.add("Group");
+
+        win.presetGroup.pdfButton = win.presetGroup.add("Button", undefined, "pdf");
+        win.presetGroup.jpgButton = win.presetGroup.add("Button", undefined, "jpg");
+
+        win.styleGroup.normalBtn = win.styleGroup.add("Button", undefined, "Normal");
+        win.styleGroup.neutralBtn = win.styleGroup.add("Button", undefined, "NEUTRAL");
+        
+        // Register event listeners that define the button behavior
+        win.presetGroup.pdfButton.onClick = function () {
+            returnValue.exportFormat = ExportFormat.PDF_TYPE;
+            returnValue.saveFile = new File(savePath + '.pdf');
+            //setPdfExportPreferences();
+            this.text = 'PDF';
+            this.parent.jpgButton.text = 'jpg';
+        }
+
+        win.presetGroup.jpgButton.onClick = function () {
+            returnValue.exportFormat = ExportFormat.JPG;
+            returnValue.saveFile = new File(savePath + '.jpg');
+            //setJpgExportPreferences();
+            this.text = 'JPG';  
+            this.parent.pdfButton.text = 'pdf';
+        }
+        
+        win.styleGroup.normalBtn.onClick = function() {
+            returnValue.neutral = false;
+            win.close();
+        };
+        win.styleGroup.neutralBtn.onClick = function() {
+            returnValue.neutral = true;
+            win.close();
+        };
+
+        // Display the window
+        win.show();
+            
+        return returnValue;
+    }
+var savePath = '~/Documents/shalala';
+var returnValue = exportStyleDialog ();
