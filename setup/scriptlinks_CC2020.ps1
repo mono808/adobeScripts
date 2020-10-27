1319042dbc9cdd3974e9ccdb825cb6869fcc5da5
Jan <mono808@gmail.com>
2020-10-27 10:18:07 +0100

    clean up

--- setup/scriptlinks_CC2020.ps1
+++
@@ -1,3 +0,0 @@
-New-Item -Path "$env:USERPROFILE\AppData\Roaming\Adobe\InDesign\Version 15.0\de_DE\Scripts\Scripts Panel\scripts" -ItemType SymbolicLink -Value "$env:pcroot/adobeScripts/indesign" -Force
-New-Item -Path "C:\Program Files\Adobe\Adobe Photoshop 2020\Presets\Scripts\scripts" -ItemType SymbolicLink -Value  "$env:pcroot/adobeScripts/Photoshop" -Force
-New-Item -Path "C:\Program Files\Adobe\Adobe Illustrator 2020\Presets\de_DE\Skripten\scripts" -ItemType SymbolicLink -Value  "$env:pcroot/adobeScripts/illustrator" -Force
