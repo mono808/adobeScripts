717ce543e427e7d942528152229524352ae6139f
Jan <mono808@gmail.com>
2020-10-27 14:06:16 +0100

    move user generated files to ~/documents/adobeScripts

--- tests/pantoneListTest.jsx
+++ tests/pantoneListTest.jsx
@@ -5,8 +5,8 @@ function setup() {
 
 }
 
-function main(modjsx) {
-    var m = require(modjsx);
+function main() {
+    var m = require('pantoneList');
     var p1 = 'PANTONE 185 C';
     var p2 = 'PANTONE 300 C';
     var p3 = 'PANTONE Orange 021 C';
@@ -16,9 +16,9 @@ function main(modjsx) {
     // m.import_pantoneList();
     // m.import_pantoneList_legacy();
     var r1 = m.rename_pantone(p1);
-    var r2 = m.rename_pantone(p1);
-    var r3 = m.rename_pantone(p2);
-    var r4 = m.rename_pantone(p2);
+    var r2 = m.rename_pantone(p2);
+    var r3 = m.rename_pantone(p3);
+    var r4 = m.rename_pantone(p4);
     
     $.writeln(r1 + '\r'+ r2);
 }
@@ -27,4 +27,4 @@ function tearDown () {
 
 }
 
-main('pantoneList');
+main();
