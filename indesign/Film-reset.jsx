c73cfecc062a251d4998692e04d6024c1d0420bf
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2020-11-16 17:15:21 +0100

    use modules

--- indesign/Film-reset.jsx
+++ indesign/Film-reset.jsx
@@ -1,46 +1,49 @@
-﻿#target indesign
-function main () {
+﻿//@target indesign
+//@include 'require.jsx'
 
-     
-    #include 'augment_objects.jsx'
-    #include 'Job.jsx'
-    #include 'JobFolder.jsx'
-    #include 'MonoNamer.jsx'
-    #include 'MonoFilm.jsx'
-    #include 'MonoMockup.jsx'
-    #include 'MonoGraphic.jsx'
-    #include 'MonoTextil.jsx'
-    #include 'MonoPrint.jsx'
-    #include 'Pathmaker.jsx'
-    #include 'PasserFab.jsx'
-    #include 'MonoSep.jsx'
-    #include 'Typeahead.jsx'
-    #include 'TexAdder.jsx'
+(function () {
 
-  
-    var job = new Job(null, false);
-    var pm = new Pathmaker(job.nfo);
+    if(!app.activeDocument) {
+        alert('Bitte zuerst Filmdatei öffnen');
+        return;
+    }
 
-    var monoFilm = new MonoFilm(app.activeDocument);
+    var job = require('job');
+    var paths = require('paths');
+    var MonoFilm = require('MonoFilm');
+
+    job.set_nfo(null, false);
+    paths.set_nfo(job.nfo);
+
+    var myDoc = app.activeDocument;
+
+    var monoFilm = new MonoFilm(myDoc);
+    var spots = monoFilm.get_all_spotColors();
+    if(spots.length < 1) {
+        alert('no spotcolors to put on film');
+        return;
+    }
+
+    try{myDoc.layers.item('motivEbene').visible = false;} catch(e){}
+    
     monoFilm.reset ();
-    
-    monoFilm.add_centermarks ();
-    
-    monoFilm.add_pictogram();    
+
+    monoFilm.add_centermarks();
+
+    monoFilm.add_pictogram();
 
     monoFilm.add_jobInfo (job);
 
-    //monoFilm.add_spotInfo_numbered ();
-
-    monoFilm.add_spotInfo_numbered ();
+    monoFilm.add_spotInfo_numbered();
     
     monoFilm.position_textFrames();
     
     monoFilm.resize_page();
 
+    try{myDoc.layers.item('motivEbene').visible = true;} catch(e){}
+
     monoFilm.save (job, true, false);
 
+    monoFilm.print(paths.path('filmIn'), paths.path('filmOut'));
 
-
-}
-main();
+})();
