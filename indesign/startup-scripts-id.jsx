d5a24e546c0b368554a83f34aaad707a18d6929f
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2020-11-19 17:47:38 +0100

    remove commented stuff

--- indesign/startup-scripts-id.jsx
+++ indesign/startup-scripts-id.jsx
@@ -2,59 +2,32 @@
 //@targetengine "session"
 //@include 'require.jsx'
 
-
 (function () {
-
-//~     function loadWorkspace (myEvent) {   
-//~         if (myEvent.parent.constructor.name !== 'LayoutWindow' ) return;
-
-//~         if(app.activeDocument.name.indexOf('Ansicht') > -1) {
-//~             try {
-//~                 app.applyWorkspace('Ansichten');
-//~             } catch(e) {
-//~                 $.writeln('could not load workspace "Ansichten"');
-//~             }
-//~         } else if (app.activeDocument.name.indexOf('Film') > -1) {
-//~             try {
-//~                 app.applyWorkspace('Filme');
-//~             } catch(e) {
-//~                 $.writeln('could not load workspace "Filme"');
-//~             }
-//~         }
-//~     }
-
-    function addToRecentFiles (myEvent) {
-        //$.writeln('event parent is ' + myEvent.parent.constructor.name);
+    function addToRecentFiles(myEvent) {
         try {
-            var doc = myEvent.parent.parent.fullName;
-            var recentFiles = require('recentFiles');
-            recentFiles.add_file(myEvent.parent.parent.fullName);
+            var currentFile = myEvent.parent.parent.fullName;
+            var recentFiles = require("recentFiles");
+            recentFiles.add_file(currentFile);
         } catch (e) {
             $.writeln(e.msg);
         }
     }
 
-    for (var i = 0; i < app.eventListeners.length; i++) {
-        var listener = app.eventListeners[i];
-        if(listener.eventType == "afterActivate") {
+    var i, listener;
+    for (i = 0; i < app.eventListeners.length; i++) {
+        listener = app.eventListeners[i];
+        if (listener.eventType == "afterActivate") {
             listener.remove();
         }
     }
 
-    for (var i = 0; i < app.eventListeners.length; i++) {
-        var listener = app.eventListeners[i];
-        if(listener.eventType == "afterOpen") {
+    for (i = 0; i < app.eventListeners.length; i++) {
+        listener = app.eventListeners[i];
+        if (listener.eventType == "afterOpen") {
             listener.remove();
         }
     }
-
-//~     app.addEventListener("afterActivate", loadWorkspace, false);
-//~     app.addEventListener("afterSave",loadWorkspace,false);
 
     app.addEventListener("afterActivate", addToRecentFiles, false);
-    app.addEventListener("afterSave",addToRecentFiles,false);
+    app.addEventListener("afterSave", addToRecentFiles, false);
 })();
-
-/* 
-alert('hello mono from indesign'); 
-*/
