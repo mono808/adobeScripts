d30e2967c0cfbb051dd36e4dc271722811c50d27
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2021-03-25 12:39:46 +0100

    wip

---
+++ indesign/Ansicht-JobInfos-eintragen.js
@@ -0,0 +1,21 @@
+﻿//@target indesign
+//@include "require.js"
+
+(function () {
+    var job = require("job");
+    //var jobFolder = require("jobFolder");
+    //var paths = require("paths");
+    var MonoMockup = require("MonoMockup");
+
+    job.set_nfo(null, false);
+    if (!job.nfo) return;
+    //paths.set_nfo(job.nfo);
+
+    var mockup = new MonoMockup();
+
+    mockup.init();
+
+    mockup.show_shop_logo(job.nfo.shop);
+
+    mockup.fill_job_infos(job.nfo);
+})();
