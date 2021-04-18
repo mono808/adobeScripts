a5af7d94d8dc6d2d7cfc798fbc5f29be11049b93
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2021-04-18 23:40:38 +0200

    only call update_stand when there is a monotable on the same page

--- indesign/Ansicht-Standautomatik.js
+++ indesign/Ansicht-Standautomatik.js
@@ -2,49 +2,42 @@
 //@targetengine session
 //@include "require.js"
 
-function show_dialog() {
-    var myDialog = app.dialogs.add({ name: "Stand kompensieren?" });
+var MonoGraphic = require("MonoGraphic");
+var MonoTable = require("MonoTable");
 
-    var myResult = myDialog.show();
-    myDialog.destroy();
+var doc = app.activeDocument;
+var infoLayer = doc.layers.item("Infos");
+var printsLayer = doc.layers.item("Prints");
 
-    return myResult;
+function update_stand (myEvent) {
+    if (app.selection.length < 1) return;
+
+    var e = myEvent;
+    var item;
+    for (var i = 0; i < app.selection.length; i++) {
+        item = app.selection[i];
+        if(!(item instanceof Rectangle || item instanceof Polygon)) return;
+        if(item.itemLayer != printsLayer) return;
+        if(item.allGraphics.length < 1) return;
+        
+        var myPage = item.parentPage;
+        var textFrame = myPage.textFrames.item("printTableFrame");
+        if(!textFrame.isValid) return;
+        
+        var myTable = textFrame.tables.item(0);
+        if(!myTable.isValid) return;
+
+        var monoGraphic = new MonoGraphic(item.allGraphics[0]);
+        if(!monoGraphic) return;
+        
+        var monoTable = new MonoTable(myPage);
+        if(!monoTable) return;
+        
+        monoTable.update_stand(monoGraphic, false);
+    }
 }
 
-function toggle_standListener(compensate) {
-    var MonoGraphic = require("MonoGraphic");
-    var MonoTable = require("MonoTable");
-
-    compensate = compensate || false;
-
-    var update_stand = function (myEvent) {
-        if (app.selection.length < 1) return;
-
-        var sel = app.selection;
-        var myPage = app.activeWindow.activePage;
-        var textFrame = infoLayer.name
-            ? myPage.textFrames.item("printTableFrame")
-            : null;
-        var myTable = textFrame.tables.item(0);
-
-        if (myTable && myTable.name) {
-            for (var i = 0; i < sel.length; i++) {
-                if (
-                    (sel[i] instanceof Rectangle ||
-                        sel[i] instanceof Polygon) &&
-                    sel[i].itemLayer == printsLayer
-                ) {
-                    var monoGraphic = new MonoGraphic(sel[i].allGraphics[0]);
-                    var monoTable = new MonoTable(myPage);
-                    monoTable.update_stand(monoGraphic, compensate);
-                }
-            }
-        }
-    };
-
-    var doc = app.activeDocument;
-    var infoLayer = doc.layers.item("Infos");
-    var printsLayer = doc.layers.item("Prints");
+function toggle_standListener() {
 
     var standListener = null;
 
@@ -65,12 +58,8 @@ function toggle_standListener(compensate) {
         standListener.remove();
         Window.alert("Stand-Automatic AUS");
     } else {
-        //compensate = show_dialog();
-        compensate = false;
         doc.addEventListener("afterSelectionAttributeChanged", update_stand);
-        compensate
-            ? Window.alert("Stand-Automatic KOMPENSIERT")
-            : Window.alert("Stand-Automatic 1:1");
+        Window.alert("Stand-Automatic AN");
     }
 }
 
