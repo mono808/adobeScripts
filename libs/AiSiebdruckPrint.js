20f4856e1a3ee147b3f853d18888a10b3cfec2cc
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2021-06-30 12:55:07 +0200

    add suspicious items to selection then confirm

--- libs/AiSiebdruckPrint.js
+++ libs/AiSiebdruckPrint.js
@@ -1,85 +1,88 @@
-﻿var AiSiebdruck = require('AiSiebdruck');
-var pantoneList = require('pantoneList');
-var MonoSpot = require('MonoSpot');
-var saveOptions = require('saveOptions');
+﻿var AiSiebdruck = require("AiSiebdruck");
+var pantoneList = require("pantoneList");
+var MonoSpot = require("MonoSpot");
+var saveOptions = require("saveOptions");
 
-function AiSiebdruckPrint (initDoc) {
+function AiSiebdruckPrint(initDoc) {
     AiSiebdruck.call(this, initDoc);
 
-    this.pantoneTxt = new File($.getenv('pcroot') + '/adobescripts/pantones.txt');
-
-
+    this.pantoneTxt = new File($.getenv("pcroot") + "/adobescripts/pantones.txt");
 }
 AiSiebdruckPrint.prototype = Object.create(AiSiebdruck.prototype);
 AiSiebdruckPrint.prototype.constructor = AiSiebdruckPrint;
 
-AiSiebdruckPrint.prototype.check = function (items)
-{
+AiSiebdruckPrint.prototype.check = function (items) {
     //separationReport
     var suspItems = {
-        nonSpotFills : [],
-        nonSpotStrokes : [],
-        spotStrokes : [],
+        nonSpotFills: [],
+        nonSpotStrokes: [],
+        spotStrokes: []
     };
 
-    if(this.pathItems.length < 1 && this.rasterItems.length < 1 && this.pageItems.length < 1) return false;
+    if (this.pathItems.length < 1 && this.rasterItems.length < 1 && this.pageItems.length < 1)
+        return false;
 
-    if(this.pathItems.length > 0) {
-        var i = this.pathItems.length-1;
+    if (this.pathItems.length > 0) {
+        var i = this.pathItems.length - 1;
         do {
             var pI = this.pathItems[i];
             // check fillcolor for spot / nonspot color
-            switch(pI.fillColor.constructor.name) {
-                case 'GrayColor' :
-                case 'LabColor' :
-                case 'RGBColor' :
-                case 'CMYKColor' :
-                case 'PatternColor' :
+            switch (pI.fillColor.constructor.name) {
+                case "GrayColor":
+                case "LabColor":
+                case "RGBColor":
+                case "CMYKColor":
+                case "PatternColor":
                     suspItems.nonSpotFills.push(pI);
-                    if(pI.stroked && pI.strokeColor.constructor.name === 'SpotColor') {
+                    if (pI.stroked && pI.strokeColor.constructor.name === "SpotColor") {
                         suspItems.spotStrokes.push(pI);
                     }
-                break;
-                case 'SpotColor' :
+                    break;
+                case "SpotColor":
                     // if pI has a stroke and is filled with sth. other than underbase spotcolor
                     // check the stroke too
-                    if (pI.stroked &&
-                        pI.strokeColor.constructor.name !== 'NoColor' &&
-                        !this.ubRegEx.test(pI.fillColor.spot.name))
-                    {
-                        if(pI.strokeColor.constructor.name === 'SpotColor') {
+                    if (
+                        pI.stroked &&
+                        pI.strokeColor.constructor.name !== "NoColor" &&
+                        !this.ubRegEx.test(pI.fillColor.spot.name)
+                    ) {
+                        if (pI.strokeColor.constructor.name === "SpotColor") {
                             suspItems.spotStrokes.push(pI);
                         } else {
                             suspItems.nonSpotStrokes.push(pI);
                         }
                     }
-                break;
+                    break;
             }
         } while (i--);
     }
 
-    if( suspItems.nonSpotFills.length > 0 ||
-        suspItems.nonSpotStrokes.length > 0 ||
-        suspItems.spotStrokes.length > 0 )
-    {
-        var cfStr = 'Separation enthält:\n\n';
-        if(suspItems.nonSpotFills.length > 0)   cfStr += suspItems.nonSpotFills.length + ' PathItems with NONSPOT FILL\n';
-        if(suspItems.nonSpotStrokes.length > 0) cfStr += suspItems.nonSpotStrokes.length + ' PathItems with NONSPOT STROKE\n';
-        if(suspItems.spotStrokes.length > 0)    cfStr += suspItems.spotStrokes.length + ' PathItems with SPOT STROKES\n';
-        cfStr += '\nContinue?';
+    if (
+        suspItems.nonSpotFills.length == 0 &&
+        suspItems.nonSpotStrokes.length == 0 &&
+        suspItems.spotStrokes.length == 0
+    )
+        return true;
 
-        if(Window.confirm(cfStr)){
-            return true;
-        } else {
-            return false;
+    var confirmer = function (suspItems) {
+        for (var key in suspItems) {
+            if (Object.hasOwnProperty.call(suspItems, key)) {
+                var items = suspItems[key];
+                if (items.length == 0) continue;
+                app.selection = items;
+                app.redraw();
+                if (!Window.confirm("selected items contain " + key + "\n\nContinue?"))
+                    return false;
+            }
         }
-    }
 
-    return true;
+        return true;
+    };
+
+    return confirmer(suspItems);
 };
 
-AiSiebdruckPrint.prototype.get_totalArea = function ()
-{
+AiSiebdruckPrint.prototype.get_totalArea = function () {
     //               1                    +
     //   bounds:  0     2     values:  -     +
     //               3                    -
@@ -89,55 +92,55 @@ AiSiebdruckPrint.prototype.get_totalArea = function ()
 
     for (var i = 0; i < this.spots.length; i++) {
         var spotChan = this.spots[i];
-        if(!spotChan.isUB) {
-            if(!initialized) {
+        if (!spotChan.isUB) {
+            if (!initialized) {
                 totalBounds = spotChan.bounds;
                 initialized = true;
                 continue;
             }
 
             var gB = spotChan.bounds;
-            if(gB[0] < totalBounds[0]) totalBounds[0] = gB[0];
-            if(gB[1] > totalBounds[1]) totalBounds[1] = gB[1];
-            if(gB[2] > totalBounds[2]) totalBounds[2] = gB[2];
-            if(gB[3] < totalBounds[3]) totalBounds[3] = gB[3];
+            if (gB[0] < totalBounds[0]) totalBounds[0] = gB[0];
+            if (gB[1] > totalBounds[1]) totalBounds[1] = gB[1];
+            if (gB[2] > totalBounds[2]) totalBounds[2] = gB[2];
+            if (gB[3] < totalBounds[3]) totalBounds[3] = gB[3];
         }
     }
 
-    var totalArea = (totalBounds[2]-totalBounds[0]) * (totalBounds[1]-totalBounds[3]);
+    var totalArea = (totalBounds[2] - totalBounds[0]) * (totalBounds[1] - totalBounds[3]);
     this.totalBounds = totalBounds;
     this.totalArea = totalArea * this.sqpt2sqcm;
 };
 
-AiSiebdruckPrint.prototype.change_fillColor = function (itemsToCheck, oldSpot, newSpot)
-{
+AiSiebdruckPrint.prototype.change_fillColor = function (itemsToCheck, oldSpot, newSpot) {
     var tempColor = new SpotColor();
     tempColor.spot = newSpot;
 
-    var i = itemsToCheck.length-1;
-    var pI, tintValue, remainingItems = [];
+    var i = itemsToCheck.length - 1;
+    var pI,
+        tintValue,
+        remainingItems = [];
 
     do {
         var pI = itemsToCheck[i];
         if (pI.fillColor.spot === oldSpot) {
-            tintValue = pI.fillColor.tint
+            tintValue = pI.fillColor.tint;
             pI.fillColor = tempColor;
             pI.fillColor.tint = tintValue;
         } else {
             remainingItems.push(pI);
         }
-    } while(i--);
+    } while (i--);
 
     return remainingItems;
 };
 
-AiSiebdruckPrint.prototype.rename_pantone_colors = function ()
-{
-    for (var i = 0, maxI = this.doc.spots.length; i < maxI; i+=1) {
+AiSiebdruckPrint.prototype.rename_pantone_colors = function () {
+    for (var i = 0, maxI = this.doc.spots.length; i < maxI; i += 1) {
         var spot = this.doc.spots[i];
         var oldSpotName = spot.name;
-        var newSpotName = pantoneList.rename_pantone(oldSpotName,spot.color);
-        if(newSpotName != oldSpotName) {
+        var newSpotName = pantoneList.rename_pantone(oldSpotName, spot.color);
+        if (newSpotName != oldSpotName) {
             spot.name = newSpotName;
         }
     }
@@ -146,16 +149,16 @@ AiSiebdruckPrint.prototype.rename_pantone_colors = function ()
 AiSiebdruckPrint.prototype.make = function (saveFile, saveOptions) {
     this.sort_by_spotColor(this.pathItems);
 
-    this.fit_artboard_to_art('Motiv');
+    this.fit_artboard_to_art("Motiv");
 
-    this.delete_layer('BG');
-    
-    if(this.pathItems.length > 0) this.rename_pantone_colors(this.pathItems);
+    this.delete_layer("BG");
+
+    if (this.pathItems.length > 0) this.rename_pantone_colors(this.pathItems);
 
     // delete fluff and save final separation for film output
-    app.doScript('Delete Fluff', 'Separation');
+    app.doScript("Delete Fluff", "Separation");
 
-    this.save_doc (saveFile, saveOptions, false);
-}
+    this.save_doc(saveFile, saveOptions, false);
+};
 
 module.exports = AiSiebdruckPrint;
