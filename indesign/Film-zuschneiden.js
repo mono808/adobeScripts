76d95f4eb8f6ee4a56175b0045202ed632ea5afa
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2021-07-29 17:49:38 +0200

    include printing film after resizing the arboard

--- indesign/Film-zuschneiden.js
+++ indesign/Film-zuschneiden.js
@@ -12,4 +12,10 @@
     var myFilm = new MonoFilm(app.activeDocument);
 
     myFilm.resize_page();
+
+    // Film drucken
+    var paths = require("paths");
+    var scriptName = "indesign/Film-drucken.js";
+    var scriptFile = new File(paths.pcroot + "/adobescripts/" + scriptName);
+    app.doScript(scriptFile);
 })();
