3e4a823fab49891616a8c925ef24f8d2338e9ae5
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2021-07-29 17:47:25 +0200

    print mockups with exact scaling to DIN A4

---
+++ indesign/Ansicht-drucken.js
@@ -0,0 +1,25 @@
+﻿//@target indesign
+//@include "require.js"
+
+(function () {
+    var interactSwitch = require("InteractionSwitch");
+    interactSwitch.set("all");
+
+    var printPreset;
+    printPreset = app.printerPresets.item("printMockup");
+
+    var doc = app.activeDocument;
+    var pageWidth = doc.documentPreferences.pageWidth;
+    var scale = (297 / pageWidth) * 100;
+
+    printPreset.scaleMode = ScaleModes.SCALE_WIDTH_HEIGHT;
+    printPreset.scaleProportional = true;
+    printPreset.scaleWidth = scale;
+    printPreset.tile = false;
+    printPreset.pagePosition = PagePositions.CENTERED;
+
+    doc.print(false, printPreset);
+    //doc.close(SaveOptions.NO);
+
+    interactSwitch.set("all");
+})();
