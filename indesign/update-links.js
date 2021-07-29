6165576a0011cdad9ac45294f9f13e38b7698857
DC-KRUEGER\jan.untiedt <mono808@gmail.com>
2021-07-29 17:50:40 +0200

    script to update the links in all indd files after jobfolder has been copied to new location

---
+++ indesign/update-links.js
@@ -0,0 +1,52 @@
+﻿//@target indesign
+//@include "require.js"
+
+var ioFile = require("ioFile");
+
+function make_array(collection) {
+    var myArray = [];
+    for (var index = 0; index < collection.length; index++) {
+        var element = collection[index];
+        myArray.push(element);
+    }
+    return myArray;
+}
+
+function modify_collection(collection, modify) {
+    collection.forEach(function (elem) {
+        modify(elem);
+    });
+
+    return collection;
+}
+
+var doc = app.activeDocument;
+var docFolder = app.activeDocument.filePath;
+
+var jobFiles = ioFile.get_files(docFolder.parent);
+var inddFiles = jobFiles.filter(function (file) {
+    return file.displayName.indexOf(".indd") > -1;
+});
+
+inddFiles.forEach(function (inddFile) {
+    var inddFileStrings = inddFile.fsName.split("\\");
+    var multiLinePath = inddFileStrings.join("\n");
+    if (!Window.confirm("Do you want to update the links in:" + multiLinePath)) return;
+    var myDoc = app.open(inddFile);
+    var myLinks = make_array(myDoc.links);
+
+    myLinks.forEach(function (link) {
+        var localFile = jobFiles.find(function (elem) {
+            return elem.displayName == link.name;
+        });
+        if (localFile) {
+            var fileStrings = localFile.fsName.split("\\");
+            var msg = "Do you want to relink '" + link.name + "' to:";
+            msg += "\n\n";
+            msg += fileStrings.join("\n");
+            if (Window.confirm(msg)) link.relink(localFile);
+        }
+    });
+    myDoc.save();
+    myDoc.close();
+});
