847429c8e58909e588bd90608c79898417ab6c69
mono808 <35834167+mono808@users.noreply.github.com>
2021-09-29 12:59:53 +0200

    script for quick export of jpg

---
+++ illustrator/export-for-screens-jpg.jsx
@@ -0,0 +1,19 @@
+﻿(function () {
+    var doc = app.activeDocument;
+    var exportFolder = doc.fullName;
+    var exportFormat = ExportForScreensType.SE_JPEG80;
+    var item = new ExportForScreensItemToExport();
+    item.document = true;
+    item.artboards = "";
+    var prefix = "";
+       
+    var opts = new ExportForScreensOptionsJPEG();
+    opts.antiAliasing = AntiAliasingMethod.TYPEOPTIMIZED;
+    opts.compressionMethod = JPEGCompressionMethodType.PROGRESSIVE;
+    opts.embedICCProfile = true;
+    opts.scaleType = ExportForScreensScaleType.SCALEBYFACTOR;
+    opts.scaleTypeValue = 0.5;
+    
+    doc.exportForScreens(exportFolder, exportFormat, opts, item, prefix);
+    
+})() 
---
+++ illustrator/export-jpg.jsx
@@ -0,0 +1,23 @@
+﻿(function () {
+    var doc = app.activeDocument;
+    var fsName = doc.fullName.fsName;
+    var extension = fsName.substring(fsName.lastIndexOf ('.'), fsName.length);
+    var exportFile = fsName.replace(extension, '.jpg');
+    
+    var lightGrey = new RGBColor();
+    lightGrey.red = 200;
+    lightGrey.green = 200;
+    lightGrey.blue = 200;
+    
+    var jpgOpts = new ExportOptionsJPEG();
+    jpgOpts.antiAliasing = true;
+    jpgOpts.qualitySetting = 60;
+    jpgOpts.artBoardClipping = true;
+    jpgOpts.matte = false;
+    jpgOpts.matteColor = lightGrey;
+    jpgOpts.horizontalScale = 50;
+    jpgOpts.verticalScale = 50;
+    
+    doc.exportFile(File(exportFile),ExportType.JPEG, jpgOpts);
+    
+})()
