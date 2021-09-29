839129e15bbc8ed635b6da55883412bb7db0082d
mono808 <35834167+mono808@users.noreply.github.com>
2021-09-29 13:28:34 +0200

    deactivate startup scripts

--- indesign/startup-scripts-id.jsx
+++ indesign/startup-scripts-id.jsx
@@ -2,32 +2,32 @@
 //@targetengine "session"
 //@include "require.js"
 
-(function () {
-    function addToRecentFiles(myEvent) {
-        try {
-            var currentFile = myEvent.parent.parent.fullName;
-            var recentFiles = require("recentFiles");
-            recentFiles.add_file(currentFile);
-        } catch (e) {
-            $.writeln(e.msg);
-        }
-    }
+//~ (function () {
+//~     function addToRecentFiles(myEvent) {
+//~         try {
+//~             var currentFile = myEvent.parent.parent.fullName;
+//~             var recentFiles = require("recentFiles");
+//~             recentFiles.add_file(currentFile);
+//~         } catch (e) {
+//~             $.writeln(e.msg);
+//~         }
+//~     }
 
-    var i, listener;
-    for (i = 0; i < app.eventListeners.length; i++) {
-        listener = app.eventListeners[i];
-        if (listener.eventType == "afterActivate") {
-            listener.remove();
-        }
-    }
+//~     var i, listener;
+//~     for (i = 0; i < app.eventListeners.length; i++) {
+//~         listener = app.eventListeners[i];
+//~         if (listener.eventType == "afterActivate") {
+//~             listener.remove();
+//~         }
+//~     }
 
-    for (i = 0; i < app.eventListeners.length; i++) {
-        listener = app.eventListeners[i];
-        if (listener.eventType == "afterOpen") {
-            listener.remove();
-        }
-    }
+//~     for (i = 0; i < app.eventListeners.length; i++) {
+//~         listener = app.eventListeners[i];
+//~         if (listener.eventType == "afterOpen") {
+//~             listener.remove();
+//~         }
+//~     }
 
-    app.addEventListener("afterActivate", addToRecentFiles, false);
-    app.addEventListener("afterSave", addToRecentFiles, false);
-})();
+//~     app.addEventListener("afterActivate", addToRecentFiles, false);
+//~     app.addEventListener("afterSave", addToRecentFiles, false);
+//~ })();
