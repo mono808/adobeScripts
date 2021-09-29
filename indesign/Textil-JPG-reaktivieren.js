ec0ed282a99ec9fdbfeeb7e220c1a66065a87150
mono808 <35834167+mono808@users.noreply.github.com>
2021-09-29 13:38:28 +0200

    script to replace flattened jpg with source file

---
+++ indesign/Textil-JPG-reaktivieren.js
@@ -0,0 +1,22 @@
+﻿//@target indesign
+//@include "require.js"
+$.level = 1;
+
+(function () {
+    if (app.selection.length < 1) {
+        alert("Please open a document first");
+        return;
+    }
+
+    var texTool = require("textilTool");
+
+    //texTool.add_textile();
+    texTool.reactivate_jpg(app.selection);
+
+    // var msg = "Textil als JPG einbetten?";
+    // var flatten = Window.confirm(msg, true, "Einbetten?");
+
+    // if (flatten) {
+    //     texTool.flatten_textile(app.selection);
+    // }
+})();
