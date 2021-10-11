811e4c776477c7d41b8c39bd3627b0fd2a334841
mono808 <35834167+mono808@users.noreply.github.com>
2021-10-11 15:21:35 +0200

    add necessary env variables

--- setup/scriptlinks_CC2018_capristuff.ps1
+++ setup/scriptlinks_CC2018_capristuff.ps1
@@ -1,3 +1,7 @@
 New-Item -Path "$env:USERPROFILE\AppData\Roaming\Adobe\InDesign\Version 13.0\de_DE\Scripts\Scripts Panel\scripts" -ItemType SymbolicLink -Value "c:/capristuff/adobeScripts/indesign" -Force
 New-Item -Path "C:\Program Files\Adobe\Adobe Photoshop CC 2018\Presets\Scripts\scripts" -ItemType SymbolicLink -Value  "c:/capristuff/adobeScripts/Photoshop" -Force
 New-Item -Path "C:\Program Files\Adobe\Adobe Illustrator CC 2018\Presets\de_DE\Skripten\scripts" -ItemType SymbolicLink -Value  "c:/capristuff/adobeScripts/illustrator" -Force
+
+[System.Environment]::SetEnvironmentVariable('csroot','\\cs-server16\CapriShare',[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('pcroot','c:\capristuff\',[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('JSINCLUDE','c:\capristuff\adobescripts\includes',[System.EnvironmentVariableTarget]::Machine)
