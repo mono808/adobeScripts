1eac1e689d94153984d953a6f4a12e1d0f5d9d77
mono808 <35834167+mono808@users.noreply.github.com>
2022-06-10 11:09:58 +0200

    script to quickly export a selection as jpg

---
+++ indesign/Grafik-exportieren.jsx
@@ -0,0 +1,37 @@
+﻿(function () {
+    
+    var disable_standAutomatik = function () {
+
+        var standListener = null;
+
+        for (var i = 0; i < app.eventListeners.length; i++) {
+            var listener = app.eventListeners[i];
+            if (listener.eventType == "afterSelectionAttributeChanged") {
+                listener.remove();
+                Window.alert("Stand-Automatic AUS");
+            }
+        }
+        
+        
+        var doc = app.activeDocument || null;
+        if(!doc) return;
+        
+        for (i = 0; i < doc.eventListeners.length; i++) {
+            listener = doc.eventListeners[i];
+            if (listener.eventType == "afterSelectionAttributeChanged") {
+                listener.remove();
+                Window.alert("Stand-Automatic AUS");
+            }
+        }
+    };
+
+    disable_standAutomatik ();
+
+    if(app.selection.length !== 2) return;
+    var docFile = app.activeDocument.fullName;
+    var page = app.activeWindow.activePage;
+    var tempGroup = page.groups.add(app.selection);
+    tempGroup.exportFile(ExportFormat.JPG, docFile.saveDlg ('Dateinamen wählen', '*.jpg'), true);
+    tempGroup.ungroup();
+    
+})();
