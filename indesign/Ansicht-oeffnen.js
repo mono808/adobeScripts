88b850c70250f801ea4a833b1f64f8a251dc584c
mono808 <35834167+mono808@users.noreply.github.com>
2022-06-27 13:37:53 +0200

    script for open mockup via jobnr

---
+++ indesign/Ansicht-oeffnen.js
@@ -0,0 +1,23 @@
+﻿//@target indesign
+//@include "require.js"
+
+(function () {
+    var _ = require("_");
+    var paths = require("paths");
+    var job = require("job");
+
+    var jobNfo = job.get_jobNfo();
+    if (!jobNfo) return;
+
+    paths.set_nfo(jobNfo);
+    var mockupFile = paths.file("mockUpIndd");
+
+    // if file does not exist, try selecting from parent folder.
+    if (!_.validate_file_ref(mockupFile)) {
+        mockupFile = File(mockupFile.parent).openDlg();
+    }
+
+    if(!mockupFile) return;
+
+    app.open(mockupFile);
+})();
