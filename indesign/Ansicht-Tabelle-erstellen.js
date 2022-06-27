9048b159abd96ebd8c70698326932bb9a8a57e10
mono808 <35834167+mono808@users.noreply.github.com>
2022-06-27 13:48:15 +0200

    remove unnecessary imports

--- indesign/Ansicht-Tabelle-erstellen.js
+++ indesign/Ansicht-Tabelle-erstellen.js
@@ -2,30 +2,21 @@
 //@include "require.js"
 
 (function () {
-    var f_all = require("f_all");
-    var jobFolder = require("jobFolder");
-    var paths = require("paths");
-    var names = require("names");
-
     var job = require("job");
-    //var print = require("print");
+    var paths = require("paths");
+
     var jobNfo = job.get_jobNfo(app.activeDocument);
-    //var printNfo = print.get_printNfo(jobNfo.ref);
+
     paths.set_nfo(jobNfo);
-    //paths.set_nfo(printNfo);
 
     var MonoMockup = require("MonoMockup");
     var MonoTable = require("MonoTable");
-    var saveOptions = require("saveOptions");
 
     var monoMockup = new MonoMockup();
     monoMockup.init(app.activeDocument);
 
     var myPage = app.activeWindow.activePage;
-    var monoGraphics = monoMockup.get_monoGraphics(
-        myPage,
-        monoMockup.layers.prints
-    );
+    var monoGraphics = monoMockup.get_monoGraphics(myPage, monoMockup.layers.prints);
 
     var monoTable = new MonoTable(myPage);
     monoTable.create_table(myPage, true);
